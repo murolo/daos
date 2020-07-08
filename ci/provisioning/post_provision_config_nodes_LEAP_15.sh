e62dbccaa8656db0b4302beee5d66394c10aec12
Brian J. Murrell <brian.murrell@intel.com>
2020-07-08 13:55:49 -0400

    Refresh group repo
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- ci/provisioning/post_provision_config_nodes_LEAP_15.sh
+++ ci/provisioning/post_provision_config_nodes_LEAP_15.sh
@@ -25,9 +25,7 @@ post_provision_config_nodes() {
          # rm -f /etc/yum.repos.d/*"$DAOS_STACK_GROUP_REPO"
         zypper --non-interactive ar "$REPOSITORY_URL"/"$DAOS_STACK_GROUP_REPO" daos-stack-group-repo
         zypper --non-interactive mr --gpgcheck-allow-unsigned-repo daos-stack-group-repo
-        # Group repo currently needs this key.
-        rpm --import 'https://download.opensuse.org/repositories/science:/HPC/openSUSE_Leap_15.1/repodata/repomd.xml.key' ||
-        rpm --import 'https://provo-mirror.opensuse.org/repositories/science:/HPC/openSUSE_Leap_15.1/repodata/repomd.xml.key'
+        zypper --non-interactive --gpg-auto-import-keys --no-gpg-checks ref daos-stack-group-repo
     fi
     
     if [ -n "$DAOS_STACK_LOCAL_REPO" ]; then
